6e7184ff1a47c8e4364c4c40c99b3ee78861ca7b
ayush <ayushsingh1372002@gmail.com>
2025-09-26 00:25:33 +0530

    connection

--- src/api.js
+++ src/api.js
@@ -1,22 +1,9 @@
-// import axios from "axios";
 
-// const API = axios.create({
-//   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
-//   headers: { "Content-Type": "application/json" }
-// });
-
-// API.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("token");
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default API;
 
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://expanse-management-backend.vercel.app/api",
   headers: { "Content-Type": "application/json" },
 });
 
